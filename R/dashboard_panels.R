4bdc1a256717020b70d8f0336160a80c48822178
Rich Bielby <richbielby@e.email>
2025-06-23 12:02:27 +0100

    Added warning styling for caution text

--- R/dashboard_panels.R
+++ R/dashboard_panels.R
@@ -1,106 +1,102 @@
 homepage_panel <- function() {
   gov_row(
     h1("NEET and participation LA scorecard"),
-    card(
-      width = 12,
-      card_title(h2("Introduction")),
-      card_body(
-        p("The Department for Education (DfE) publishes an estimate each year
+    h2("Introduction"),
+    p("The Department for Education (DfE) publishes an estimate each year
                 of the proportion of young people not in education, employment or
                 training (NEET).  However, evidence shows that there are a range of
                 factors that can affect the proportion NEET, and this scorecard aims
                 to put the headline figure into context by presenting it alongside a
                 range of other related information."),
-        p("There is a separate scorecard for each local authority in England
+    p("There is a separate scorecard for each local authority in England
                 except the City of London and Isles of Scilly. For these two local authorities, data is not available
                 for all indicators and small numbers can result in large changes
                 in percentages from one year to the next."),
-        p("Each area's performance is compared with that of other local authorities in England.
+    p("Each area's performance is compared with that of other local authorities in England.
             The best performing with the lowest NEET/highest participation fall into the
             green quintile, whereas local authorities with the highest NEET/lowest participation fall
             into the red quintile."),
-        p(strong("Some caution should be taken if using these figures due to the estimates being based on
+    warning_text(
+      inputId = "vatiation_warning",
+      text = "Some caution should be taken if using these figures due to the estimates being based on
             management information. There is considerable variation at local authority level in
             how well 16 and 17 year olds are tracked and hence 'activity not known' proportions can impact on the
-            estimates of the proportion NEET.")),
-        # p(strong("The Department for Education's definitive measures for England
-        # of participation and not in education, employment or training (NEET) for 16 to 18 year olds are
-        # published annually in the national statistics release", a(
-        # href = "https://explore-education-statistics.service.gov.uk/find-statistics/participation-in-education-and-training-and-employment",
-        # "'Participation in Education, Training and Employment age 16 to 18'."
-        # )))
-      )
+            estimates of the proportion NEET."
     ),
+    # p(strong("The Department for Education's definitive measures for England
+    # of participation and not in education, employment or training (NEET) for 16 to 18 year olds are
+    # published annually in the national statistics release", a(
+    # href = "https://explore-education-statistics.service.gov.uk/find-statistics/participation-in-education-and-training-and-employment",
+    # "'Participation in Education, Training and Employment age 16 to 18'."
+    # )))
 
     ## Left panel -------------------------------------------------------
 
-    card(
-      width = 12,
-      card_title(h2("Scorecard Contents")),
-      card_body(
-        h3("NEET and activity not known"),
-        p("This section shows the proportion of 16 and 17 year olds living in each area who were not in education,
+    h2("Scorecard Contents"),
+    h3("NEET and activity not known"),
+    p("This section shows the proportion of 16 and 17 year olds living in each area who were not in education,
                        employment or training (NEET) or their activity was not known (NK) at the end of the year."),
-        p("The proportion not known indicator shows how effective the arrangements are in each area for tracking
+    p("The proportion not known indicator shows how effective the arrangements are in each area for tracking
                        young people's participation in education and training."),
-        p("Whilst local authorities are responsible for tracking, they depend on schools, colleges and other partners
+    p("Whilst local authorities are responsible for tracking, they depend on schools, colleges and other partners
                        who work with young people sharing information with them. There is a risk that young people NEET have not been
                        identified in areas where effective tracking arrangements are not in place."),
-        h3("Vulnerable groups NEET"),
-        p("This section includes data showing the proportion of 16 and 17 year olds living in each area who were not in education,
-                       employment or training (NEET) or their activity was not known (NK) at the end of the year with special education needs
-                       and disability (SEND)/education, health and care plan (EHCP), SEN support or those that fall into the vulnerable group."),
-        p("A young person is said to be in a vulnerable group if they have any of the following characteristics (taken from IC01 of
-                   the NCCIS returns):"),
-        tags$ul(
-          tags$li("110 - Looked after / In care"),
-          tags$li("130 - Refugee / Asylum seeker"),
-          tags$li("140 - Carer-not own child"),
-          tags$li("150 - Disclosed substance misuse"),
-          tags$li("160 - Care leaver"),
-          tags$li("170 - Supervised by YOT (Youth Offending Team)"),
-          tags$li("190 - Parent-not caring for own child"),
-          tags$li("200 - Alternative provision"),
-          tags$li("210 - Mental health flag")
-        ),
-        p(strong("Caution should be used in interpreting these figures due to variation in local authority reporting of these characteristics.
-                    The proportion of the 16/17 cohort classified as being in the vulnerable group ranges from 0.0 percent to 10.3 percent.
-                   Due to this variation and likely inaccuracy in some local authorities in identifying the full vulnerable group cohort,
-                   NEET/not known rates may not be representative for the vulnerable group cohort. The underlying data accompanying this scorecard
-                   includes the proportion of each local authority's cohort identified as having one of the vulnerable characteristics (VG_cohort_percentage).")),
-        h3("Participation"),
-        p("This section shows the proportion of 16 and 17 year olds living in each area who were in education or training
+    h3("Participation"),
+    p("This section shows the proportion of 16 and 17 year olds living in each area who were in education or training
                        at the end of March."),
-        p("There is a breakdown of the three main routes that young people choose: full-time education, apprenticeship, and other education or training
+    p("There is a breakdown of the three main routes that young people choose: full-time education, apprenticeship, and other education or training
                        (this includes part-time education, work based learning and employment with study towards a regulated qualification)."),
-        p("It also shows the proportion of 16 and 17 year olds receiving an offer of a place in education and training under the September
+    p("It also shows the proportion of 16 and 17 year olds receiving an offer of a place in education and training under the September
                        Guarantee. The September Guarantee is a guarantee of an offer, made by the end of September, of an
                       appropriate place in post-16 education or training for every young person completing compulsory
                       education. This is particularly important as it helps young people make a seamless transition into
                       post-16 learning or employment with training. Local authorities are responsible for leading the 'September Guarantee' process, working with schools and colleges
                        across their area."),
-        p("There are some young people who have not yet made a decision about what they want to do next, have other plans, or who cannot be
+    p("There are some young people who have not yet made a decision about what they want to do next, have other plans, or who cannot be
                        contacted. These young people are at risk of becoming NEET."),
-        h3("Contextual - attainment and attendence"),
-        p(
-          "This section covers ",
-          external_link(href = "https://explore-education-statistics.service.gov.uk/find-statistics/level-2-and-3-attainment-by-young-people-aged-19/2022-23", "post 16 attainment"), ", ",
-          external_link(href = "https://explore-education-statistics.service.gov.uk/find-statistics/key-stage-4-performance-revised/2022-23", "GCSE attainment"), " and ",
-          external_link(href = "https://explore-education-statistics.service.gov.uk/find-statistics/pupil-absence-in-schools-in-england/2022-23", "school attendance"), " of young people living in each area.
+    h3("Vulnerable groups NEET"),
+    p("This section includes data showing the proportion of 16 and 17 year olds living in each area who were not in education,
+                       employment or training (NEET) or their activity was not known (NK) at the end of the year with special education needs
+                       and disability (SEND)/education, health and care plan (EHCP), SEN support or those that fall into the vulnerable group."),
+    p("A young person is said to be in a vulnerable group if they have any of the following characteristics (taken from IC01 of
+                   the NCCIS returns):"),
+    tags$ul(
+      tags$li("110 - Looked after / In care"),
+      tags$li("130 - Refugee / Asylum seeker"),
+      tags$li("140 - Carer-not own child"),
+      tags$li("150 - Disclosed substance misuse"),
+      tags$li("160 - Care leaver"),
+      tags$li("170 - Supervised by YOT (Youth Offending Team)"),
+      tags$li("190 - Parent-not caring for own child"),
+      tags$li("200 - Alternative provision"),
+      tags$li("210 - Mental health flag")
+    ),
+    warning_text(
+      inputId = "vulnerablecaution",
+      text = "Caution should be used in interpreting these figures due to variation in local authority reporting of these characteristics.
+                    The proportion of the 2016/17 cohort classified as being in the vulnerable group ranges from 0.0 percent to 10.3 percent.
+                   Due to this variation and likely inaccuracy in some local authorities in identifying the full vulnerable group cohort,
+                   NEET / not known rates may not be representative for the vulnerable group cohort. The underlying data accompanying this scorecard
+                   includes the proportion of each local authority's cohort identified as having one of the vulnerable characteristics (VG_cohort_percentage)."
+    ),
+    h3("Contextual - attainment and attendence"),
+    p(
+      "This section covers ",
+      external_link(href = "https://explore-education-statistics.service.gov.uk/find-statistics/level-2-and-3-attainment-by-young-people-aged-19/2022-23", "post 16 attainment"), ", ",
+      external_link(href = "https://explore-education-statistics.service.gov.uk/find-statistics/key-stage-4-performance-revised/2022-23", "GCSE attainment"), " and ",
+      external_link(href = "https://explore-education-statistics.service.gov.uk/find-statistics/pupil-absence-in-schools-in-england/2022-23", "school attendance"), " of young people living in each area.
                   It also provides the Client Caseload Information System (CCIS) population of 16 and 17 year olds in the local authority."
-        ),
-        p("Surveys show that higher attainment at age 16 is the factor most closely associated with participation and a lower
+    ),
+    p("Surveys show that higher attainment at age 16 is the factor most closely associated with participation and a lower
                        risk of becoming NEET between the ages of 16 and 18."),
-        p("Young people who have poor attendance or who are excluded from schools are at greater risk of becoming NEET."),
-        # br("Due to the coronavirus (COVID-19) pandemic, all summer 2020 and 2021 exams were cancelled and absence statistics were
-        # notably different to previous years. Therefore the contextual information on absence and attainment has not been
-        # included in the scorecard this year."),
-        # tags$div(
-        # title = "This section is useful if you want to understand how well different industries retain graduates.",
-        # h3(actionLink("link_to_app_content_tab", "App Content"))
-        # ),
-      )
-    )
+    p("Young people who have poor attendance or who are excluded from schools are at greater risk of becoming NEET."),
+    # br("Due to the coronavirus (COVID-19) pandemic, all summer 2020 and 2021 exams were cancelled and absence statistics were
+    # notably different to previous years. Therefore the contextual information on absence and attainment has not been
+    # included in the scorecard this year."),
+    # tags$div(
+    # title = "This section is useful if you want to understand how well different industries retain graduates.",
+    # h3(actionLink("link_to_app_content_tab", "App Content"))
+    # ),
   )
 }
 
@@ -150,6 +146,7 @@ dashboard_panel <- function() {
         )
       )
     ),
+    br(),
     tabsetPanel(
       id = "tabsetpanel",
       tabPanel(
