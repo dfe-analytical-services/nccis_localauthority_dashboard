76b8e4628054a6a95165d7a21ae962c7275cf953
Rich Bielby <richbielby@e.email>
2025-07-16 09:24:46 +0100

    Added an LA selection to tests

--- tests/testthat/test-dashboard_ui_tests.R
+++ tests/testthat/test-dashboard_ui_tests.R
@@ -28,19 +28,23 @@ test_that("Migrated shinytest test: dashboard_ui_tests.R", {
   # 1. Does it load?
   app$expect_values(input = listInputs, output = outputs)
 
-  # 2. Does the first dashboard panel load?
+  # 2. Does the LA selection update?
+  app$set_inputs(LA_choice = "Southwark")
+  app$expect_values(input = listInputs, output = outputs)
+
+  # 3. Does the first dashboard panel load?
   app$set_inputs(tabsetpanel = "neet")
   app$expect_values(input = listInputs, output = outputs)
 
-  # 3. Does the vulnerable dashboard panel load?
+  # 4. Does the vulnerable dashboard panel load?
   app$set_inputs(tabsetpanel = "vulnerable")
   app$expect_values(input = listInputs, output = outputs)
 
-  # 4. Does the participation dashboard panel load?
+  # 5. Does the participation dashboard panel load?
   app$set_inputs(tabsetpanel = "participation")
   app$expect_values(input = listInputs, output = outputs)
 
-  # 5. Does the contextual dashboard panel load?
+  # 6. Does the contextual dashboard panel load?
   app$set_inputs(tabsetpanel = "contextual")
   app$expect_values(input = listInputs, output = outputs)
 })
