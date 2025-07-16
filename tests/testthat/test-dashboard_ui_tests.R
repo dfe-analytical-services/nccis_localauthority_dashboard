72d23767579e3acc072b5cf137a1bb208846da88
Rich Bielby <richbielby@e.email>
2025-07-16 09:06:55 +0100

    Added no-data style icon for non-numeric data values

--- tests/testthat/test-dashboard_ui_tests.R
+++ tests/testthat/test-dashboard_ui_tests.R
@@ -26,21 +26,21 @@ test_that("Migrated shinytest test: dashboard_ui_tests.R", {
   )
 
   # 1. Does it load?
-  app$expect_values(input = listInputs, output = c())
+  app$expect_values(input = listInputs, output = outputs)
 
   # 2. Does the first dashboard panel load?
-  app$set_inputs(tabsetpanel = "neet", timeout_ = 5000)
+  app$set_inputs(tabsetpanel = "neet")
   app$expect_values(input = listInputs, output = outputs)
 
   # 3. Does the vulnerable dashboard panel load?
-  app$set_inputs(tabsetpanel = "vulnerable", timeout_ = 5000)
+  app$set_inputs(tabsetpanel = "vulnerable")
   app$expect_values(input = listInputs, output = outputs)
 
   # 4. Does the participation dashboard panel load?
-  app$set_inputs(tabsetpanel = "participation", timeout_ = 5000)
+  app$set_inputs(tabsetpanel = "participation")
   app$expect_values(input = listInputs, output = outputs)
 
   # 5. Does the contextual dashboard panel load?
-  app$set_inputs(tabsetpanel = "contextual", timeout_ = 5000)
+  app$set_inputs(tabsetpanel = "contextual")
   app$expect_values(input = listInputs, output = outputs)
 })
