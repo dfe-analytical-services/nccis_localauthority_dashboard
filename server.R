b32a381fd2bc5a2199e43392fe84f892a9789ac8
BLYTH <Lucy.BLYTH@EDUCATION.GOV.UK>
2026-06-15 12:37:31 +0100

    LB_ minor modifications made to caveats

--- server.R
+++ server.R
@@ -935,7 +935,8 @@ server <- function(input, output, session) {
       title = "NCCIS population",
       value = format(NCCIS_population, big.mark = ","),
       showcase = bs_icon("person-fill", size = "2em"),
-      p("Recorded on CCIS - end ", last_year, " (average of December, January, February)"),
+      p("Recorded on CCIS - end ", last_year,"/start", latest_year),
+      p(" (average of December, January, February)"),
       theme = "blue"
     )
   })
